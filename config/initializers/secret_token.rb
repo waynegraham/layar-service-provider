d227f35434c6b4b2fd5b8e9d8bf5ecb709b5fc9e
Wayne Graham <wayne.graham@virginia.edu>
2011-04-26 19:59:13 -0400

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LayarServiceProvider3::Application.config.secret_token = '84a0de34cfcd7c707e14e10b601ce66ce1a2ea7fa67ef9d026300c39ab61a2034aca74336b394c99cdca763d59b6589ea8a9747385578c09e011a14d651c90e1'
