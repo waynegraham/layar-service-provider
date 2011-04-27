67f5ceedf9249aee3db93f9f2fbe57f15a59b712
Wayne Graham <wayne.graham@virginia.edu>
2011-04-26 21:55:19 -0400

    added readme and moved database.yml file

---
+++ README.md
@@ -0,0 +1,14 @@
+Layar Service Provider
+----------------------
+
+This is a Rails 3 implementation of the LayAR Service provider
+implementation.
+
+## Running
+
+Clone the repo, edit the database.yml and fire off the server.
+
+    git clone blah/blah/blat.git
+
+## Adding POIs
+The application contains a rudimentary admin console to add/edit POIs. 
--- app/models/point.rb
+++ app/models/point.rb
@@ -1,9 +1,10 @@
 class Point < ActiveRecord::Base
-  acts_as_mappable :auto_geocode => false,
+  acts_as_mappable :auto_geocode => {:field => :address, :error_message => 'Could not geocode address'},
                    :default_units => :miles,
                    :default_formula => :sphere,
                    :distance_field_name => :distance,
                    :lat_column_name => :lat,
                    :lng_column_name => :lng
 
+  
 end
