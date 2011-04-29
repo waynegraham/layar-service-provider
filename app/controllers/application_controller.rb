b256c6b9dfea1ac8398591a6b9601c956aeac642
Wayne Graham <wayne.graham@virginia.edu>
2011-04-29 14:31:34 -0400

    added integration for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  #check_authorization :unless => :devise_controller?
 
-  check_authorization #lock it down
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url # go to /index.html
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ LayarServiceProvider3::Application.routes.draw do
   get "pages/index"
 
   get "pages/about"
-
-  devise_for :users
-
   get "geo/index"
+  
+  devise_for :users do
+    get "sign_up_path", :to => "devise/sessions#new"
+    get 'log_in_path', :to => "devise/sessions#new"
+  end
+
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,7 +60,7 @@ LayarServiceProvider3::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#index"
 
   # See how all your routes lay out with "rake routes"
 
